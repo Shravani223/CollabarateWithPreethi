f6a364edc1adce4362edae4d50b1406557d7be4e
shravani223 <koyyanisravani98@gmail.com>
2023-08-22 23:25:12 +0530

    "G.O.A.T album by diljith dosanjh songs"

--- CollabarateWithPreethi/CollabarateWithPreethi.cpp
+++ CollabarateWithPreethi/CollabarateWithPreethi.cpp
@@ -5,5 +5,5 @@ using namespace std;
 int main()
 {
 	cout << "Arjit singh and diljith dosanjh" << endl;
-	cout << "G.O.A.T album by diljith dosanjh" << endl;
+	cout << "G.O.A.T album by diljith dosanjh songs" << endl;
 }
