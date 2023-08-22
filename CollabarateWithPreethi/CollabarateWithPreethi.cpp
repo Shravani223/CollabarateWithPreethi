1c42df74b74addd386a7062d742e918751b77110
Marvel9012 <marvelkingdom.9012@gmail.com>
2023-08-22 23:37:36 +0530

    song added.

--- CollabarateWithPreethi/CollabarateWithPreethi.cpp
+++ CollabarateWithPreethi/CollabarateWithPreethi.cpp
@@ -6,4 +6,5 @@ int main()
 {
 	cout << "Arjit singh and diljith dosanjh" << endl;
 	cout << "G.O.A.T album by diljith dosanjh songs" << endl;
+	cout << "kaisa hua kaisa hua tu itna jarur kaisa hua" << endl;
 }
