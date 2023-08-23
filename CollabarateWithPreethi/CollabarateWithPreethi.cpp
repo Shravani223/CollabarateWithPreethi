c3ca0343d73706d7eed970b54f1988b8b1b0f401
preethi <Preethiindarapu17@gmail.com>
2023-08-23 15:13:41 +0530

    Type here to search

--- CollabarateWithPreethi/CollabarateWithPreethi.cpp
+++ CollabarateWithPreethi/CollabarateWithPreethi.cpp
@@ -7,4 +7,5 @@ int main()
 	cout << "Arjit singh and diljith dosanjh" << endl;
 	cout << "G.O.A.T album by diljith dosanjh songs" << endl;
 	cout << "kaisa hua kaisa hua tu itna jarur kaisa hua." << endl;
+	cout << "Type here to search" << endl;
 }
